535189d330d1dffface8879be6239e483939fa7e
60s01 <6osa9.t9h@gmail.com>
2023-06-24 00:53:09 +0300

    testde v2

---
+++ Conference/Autorizaciya.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Conference
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorizaciya.xaml
+    /// </summary>
+    public partial class Autorizaciya : Page
+    {
+        public Autorizaciya()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var i in Entities.GetContext().Polsovateli)
+            {
+                if ((tbId.Text == Convert.ToString(i.IdPolzovatelya)) && (tbParol.Text == Convert.ToString(i.Parol)))
+                {
+                    MessageBox.Show($"Вы зарегистрировались под {i.Imya} {i.Familiya}");
+                }
+            }
+        }
+    }
+}
---
+++ Conference/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Conference
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainFrame;
+        }
+        /// <summary>
+        /// Просмотр мероприятий
+        /// </summary>
+        
+        private void EventRead_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// переход на страницу авторизации
+        /// </summary>
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Autorizaciya());
+        }
+        /// <summary>
+        /// Выйти на главную страницу
+        /// </summary>
+        private void SignOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
